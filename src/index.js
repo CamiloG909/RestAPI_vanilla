f82c3962ccd8817b3a3f8ad5687506f9583f2e0e
CamiloG <cgalindorivera@gmail.com>
2021-11-07 14:31:10 -0500

    updated folders

--- src/index.js
+++ src/index.js
@@ -1,116 +1,115 @@
-/* eslint-disable no-console */
-const http = require('http')
-const { bodyParser } = require('./lib/bodyParser')
+const http = require("http");
+const { bodyParser } = require("./lib/bodyParser");
 
-let database = []
+let database = [];
 
 function getTaskHandler(req, res) {
-	res.writeHead(200, { 'Content-Type': 'application/json' })
-	res.write(JSON.stringify(database))
-	res.end()
+	res.writeHead(200, { "Content-Type": "application/json" });
+	res.write(JSON.stringify(database));
+	res.end();
 }
 
 async function createTaskHandler(req, res) {
 	try {
-		await bodyParser(req)
-		database.push(req.body)
-		res.writeHead(200, { 'Content-Type': 'application/json' })
-		res.write(JSON.stringify(database))
-		res.end()
+		await bodyParser(req);
+		database.push(req.body);
+		res.writeHead(200, { "Content-Type": "application/json" });
+		res.write(JSON.stringify(database));
+		res.end();
 	} catch (error) {
-		res.writeHead(200, { 'Content-Type': 'text/plain' })
-		res.write('Invalid data')
-		res.end()
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.write("Invalid data");
+		res.end();
 	}
 }
 
 async function updateTaskHandler(req, res) {
 	try {
-		let { url } = req
+		let { url } = req;
 
-		let idQuery = url.split('?')[1]
-		let idKey = idQuery.split('=')[0]
-		let idValue = idQuery.split('=')[1]
+		let idQuery = url.split("?")[1];
+		let idKey = idQuery.split("=")[0];
+		let idValue = idQuery.split("=")[1];
 
-		if (idKey === 'id') {
-			await bodyParser(req)
-			database[idValue - 1] = req.body
-			res.writeHead(200, { 'Content-Type': 'text/plain' })
-			res.write('Updated succesfully')
-			res.end()
+		if (idKey === "id") {
+			await bodyParser(req);
+			database[idValue - 1] = req.body;
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			res.write("Updated succesfully");
+			res.end();
 		} else {
-			res.writeHead(200, { 'Content-Type': 'text/plain' })
-			res.write('Invalid request query')
-			res.end()
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			res.write("Invalid request query");
+			res.end();
 		}
 	} catch (error) {
-		res.writeHead(200, { 'Content-Type': 'text/plain' })
-		res.write('Invalid body data was provided', error.message)
-		res.end()
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.write("Invalid body data was provided", error.message);
+		res.end();
 	}
 }
 
 async function deleteTaskHandler(req, res) {
 	try {
-		let { url } = req
+		let { url } = req;
 
-		let idQuery = url.split('?')[1]
-		let idKey = idQuery.split('=')[0]
-		let idValue = idQuery.split('=')[1]
+		let idQuery = url.split("?")[1];
+		let idKey = idQuery.split("=")[0];
+		let idValue = idQuery.split("=")[1];
 
-		if (idKey === 'id') {
-			await bodyParser(req)
-			database.splice(idValue - 1, 1)
-			res.writeHead(200, { 'Content-Type': 'text/plain' })
-			res.write('Deleted succesfully')
-			res.end()
+		if (idKey === "id") {
+			await bodyParser(req);
+			database.splice(idValue - 1, 1);
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			res.write("Deleted succesfully");
+			res.end();
 		} else {
-			res.writeHead(200, { 'Content-Type': 'text/plain' })
-			res.write('Invalid request query')
-			res.end()
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			res.write("Invalid request query");
+			res.end();
 		}
 	} catch (error) {
-		res.writeHead(200, { 'Content-Type': 'text/plain' })
-		res.write('Invalid body data was provided', error.message)
-		res.end()
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.write("Invalid body data was provided", error.message);
+		res.end();
 	}
 }
 
 const server = http.createServer((req, res) => {
-	const { url, method } = req
+	const { url, method } = req;
 
-	console.log(`URL: ${url} - METHOD: ${method}`)
+	console.log(`URL: ${url} - METHOD: ${method}`);
 
 	switch (method) {
-		case 'GET':
-			if (url === '/') {
-				res.writeHead(200, { 'Content-Type': 'application/json' })
-				res.write(JSON.stringify({ message: 'Received' }))
-				res.end()
+		case "GET":
+			if (url === "/") {
+				res.writeHead(200, { "Content-Type": "application/json" });
+				res.write(JSON.stringify({ message: "Received" }));
+				res.end();
 			}
-			if (url === '/tasks') {
-				getTaskHandler(req, res)
+			if (url === "/tasks") {
+				getTaskHandler(req, res);
 			}
-			break
+			break;
 
-		case 'POST':
-			if (url === '/tasks') {
-				createTaskHandler(req, res)
+		case "POST":
+			if (url === "/tasks") {
+				createTaskHandler(req, res);
 			}
-			break
-		case 'PUT':
-			updateTaskHandler(req, res)
-			break
-		case 'DELETE':
-			deleteTaskHandler(req, res)
-			break
+			break;
+		case "PUT":
+			updateTaskHandler(req, res);
+			break;
+		case "DELETE":
+			deleteTaskHandler(req, res);
+			break;
 		default:
-			res.writeHead(200, { 'Content-Type': 'text/plain' })
-			res.write('Not found')
-			res.end()
-			break
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			res.write("Not found");
+			res.end();
+			break;
 	}
-})
+});
 
-server.listen(3000)
-console.log('Server on port', 3000)
+server.listen(4000);
+console.log("Server on port", 4000);
